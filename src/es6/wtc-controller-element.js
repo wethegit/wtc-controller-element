6a3b6ed04facc76c0f33a30e9943878962ad4476
Marlon Marcello <marlon@wethecollective.com>
2016-11-22 12:18:30 -0800

    Fixed export default

---
+++ src/es6/wtc-controller-element.js
@@ -0,0 +1,39 @@
+/*
+    Element Controller
+    =======================================
+    Author          liamegan
+    email           liam@wethecollective.com
+    Created         2014-03-12 08:59:26
+    namespace       com.wtc.controller
+    Requirements    jQuery
+    Description     General element controller
+    Edited by       liamegan
+    Edited          2014-05-05 16:51:10
+    Version         0.1
+*/
+class ElementController {
+  constructor(element) {
+    this.element = element;
+    this.element.data = {};
+    this.element.data.controller = this;
+    this.element.data.instanciated = true;
+  }
+
+  elementExistsInDOM() {
+    let element, exists;
+    exists = this.element && this.element[0];
+    if (!exists) {
+      return false;
+    }
+    element = this.element[0];
+    while (element) {
+      if (element === document) {
+        return true;
+      }
+      element = element.parentNode;
+    }
+    return false;
+  }
+}
+
+export default ElementController;
